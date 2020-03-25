1bd6e94902bce08dca7a749a10caeb211f5e97fe
coronaisme <coronaisme@gmail.com>
2020-03-25 14:01:56 -0700

    fixed how reviews were posted

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -15,22 +15,13 @@ class Api::V1::ReviewsController < ApplicationController
   end
 
   def create 
-    # if logged_in?
-        user = User.find_by(id:params[:user_id])
-        wine = Wine.find_by(id:params[:wine_id])
         review = Review.new(content:params[:content], wine_id:params[:wine_id], user_id:params[:user_id])
         
         if review.save 
-          render json: { id: review.id, content: review.content, user: user, wine:wine }
+          render json: { review: review, user:review.user }
         else 
           render json: { error: 'You may only write a review of a wine once!' }, status: 401
-        end
-      
-    # else
-    #   render json: { error: 'Must be logged in to create a review' }, status: 401
-    # end
+        end     
   end
-
-
 
 end
